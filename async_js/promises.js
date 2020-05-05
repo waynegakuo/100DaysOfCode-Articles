f440e1379a5fa168a5394d2c40ae082c01d79e0e
waynegakuo <waynejotham@gmail.com>
2020-05-05 12:53:21 +0300

    Promise.all to take care of all promises created

--- async_js/promises.js
+++ async_js/promises.js
@@ -39,6 +39,21 @@ function createPost(post) {
     });
 }
 
-createPost({ title: 'Post Three', body: 'This is the third post' })
-    .then(getPosts)
-    .catch(err => console.log(err));
+// createPost({ title: 'Post Three', body: 'This is the third post' })
+//     .then(getPosts)
+//     .catch(err => console.log(err));
+
+// Promise.all
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+    setTimeout(resolve, 2000, 'Goodbye')
+);
+
+const promise4 = fetch
+('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+
+// Promise.all takes in an array of promises
+// It's going to take however long the longest promise is
+Promise.all([promise1, promise2, promise3, promise4])
+    .then((values) => console.log(values))
