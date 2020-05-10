dfbc0e69c1fb7202b878fd08b6a21c45c84accb7
waynegakuo <waynejotham@gmail.com>
2020-05-10 17:21:38 +0300

    pop array method

---
+++ array_methods_js/pop.js
@@ -0,0 +1,9 @@
+// Removes the last item from an array and return it
+const numbers = [1, 2, 3, 4]
+
+console.log('Initial array before pop', numbers)
+
+const lastItem = numbers.pop()
+
+console.log('Array after pop', numbers)
+console.log('Popped item', lastItem)
