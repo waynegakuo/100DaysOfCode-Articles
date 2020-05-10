5067dd6e5bebc3177eb574e6a3f0f068ffcea26a
waynegakuo <waynejotham@gmail.com>
2020-05-10 19:12:21 +0300

    isArray array method

---
+++ array_methods_js/isArray.js
@@ -0,0 +1,8 @@
+// Checks if the passed value is an array
+
+const names = ['Florin', 'Ivan', 'Liam']
+const str = 'Hello World'
+const number = 17
+
+//isArray method leaves in the Array class so it should always follow 'Array.'
+console.log(Array.isArray(names))
