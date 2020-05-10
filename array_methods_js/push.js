2bbba47fecfb4a9991ba76cffbc19da325bcb773
waynegakuo <waynejotham@gmail.com>
2020-05-10 17:08:29 +0300

    push array method

---
+++ array_methods_js/push.js
@@ -0,0 +1,6 @@
+// Allows you to add one or more elements at the end of the array and returns the new length of the array
+const numbers = [1, 2, 3]
+const total = numbers.push(4, 5, 6, 7);
+
+console.log(numbers)
+console.log('The length of this array is: ', total)
